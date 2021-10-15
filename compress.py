db4ef4cfc0706c3b815e072601a4bab57d1170bf
yrom1 <ryanm.inbox@gmail.com>
2021-10-15 12:07:29 -0400

    Added -s --suppress flag to stop all print statements (like the default 'Compressed filesize: 1.1 MB' message).

--- compress.py
+++ compress.py
@@ -17,12 +17,14 @@ def main() -> None:
     parser.add_argument('filepath', type=file_path, help='full filepath to image')
     parser.add_argument('-q', '--quality', type=int, help='compression quality, [0, 100], default 90 (slight compression).', metavar='QUALITY', nargs='?', choices=list(range(1,101)), default=90)
     parser.add_argument('-v','--verbose', action='store_true', help='verbose filesize change information')
+    parser.add_argument('-s','--suppress', action='store_true', help='suppress print statements')
     args = parser.parse_args()
 
     image_path = args.filepath
     image_quality = args.quality
     verbose = args.verbose
-
+    suppress = args.suppress
+    
     image = Image.open(image_path)
     directory, filename = os.path.split(image_path)
     save_path = os.path.join(directory, (f'COMPRESSED-QUAL={image_quality}_{filename}'))
@@ -33,15 +35,16 @@ def main() -> None:
     compression_size_drop = original_size - compressed_size
     compression_percent_decrease = (compression_size_drop / original_size) * 100
  
-    if verbose:
-        print('Image compression level:', image_quality)
-        print('Original filesize:', str(round(original_size, 2)), 'MB')
-    print('Compressed filesize:', str(round(compressed_size, 2)), 'MB', end='')
-    
-    if verbose:
-        print(f' (-{round(compression_size_drop, 2)} MB, -{round(compression_percent_decrease, 2)}%)')
-    else:
-        print()
+    if not suppress:
+        if verbose:
+            print('Image compression level:', image_quality)
+            print('Original filesize:', str(round(original_size, 2)), 'MB')
+        print('Compressed filesize:', str(round(compressed_size, 2)), 'MB', end='')
+        
+        if verbose:
+            print(f' (-{round(compression_size_drop, 2)} MB, -{round(compression_percent_decrease, 2)}%)')
+        else:
+            print()
 
 if __name__ == '__main__':
     main()
