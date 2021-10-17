13a591ed8771910fc17f1b16fc4daf1f70cd4e30
yrom1 <ryanm.inbox@gmail.com>
2021-10-16 21:50:42 -0400

    Progressive dropping of image quaality setting seems to work on basic testing, if verbose, prints quality setting being attempted.

--- compress.py
+++ compress.py
@@ -1,10 +1,14 @@
+from abc import ABCMeta
 import os
 import sys
 import argparse
 from PIL import Image
 
 DEFAULT_QUALITY = 90
-assert DEFAULT_QUALITY >= 0 and DEFAULT_QUALITY <= 100 and DEFAULT_QUALITY % 5 == 0
+assert DEFAULT_QUALITY >= 0 and DEFAULT_QUALITY <= 100 #  PIL save image quality range is [0, 100]
+assert DEFAULT_QUALITY % 5 == 0 # make adaptive compression easier, rather than search all 100 possibilities
+SAVED_IMAGES = [] #  should only have one saved image and deleted failed attempts with adpative compression 
+CURRENT_IMAGE_QUALITY = -1
 
 def bytes_to_megabytes(bytes: int) -> int:
     """Convert bytes to mebibytes."""
@@ -58,28 +62,46 @@ def main() -> None:
     args = parser.parse_args()
 
     image_path = args.filepath
-    image_quality = args.quality
+    CURRENT_IMAGE_QUALITY = args.quality
     verbose = args.verbose
     suppress = args.suppress
     adaptive = args.adaptive
-    print(adaptive)
 
-    png_image = Image.open(image_path)
     directory, filename_with_extension = os.path.split(image_path)
     filename, extension = os.path.splitext(filename_with_extension)
-    save_path = os.path.join(
-        directory, ((f"COMPRESSED-QUAL={image_quality}_{filename}") + ".jpg")
-    )
-    jpg_image = png_image.convert("RGB")
-    jpg_image.save(save_path, quality=image_quality, optimize=True)
     original_size = bytes_to_megabytes(os.path.getsize(image_path))
-    compressed_size = bytes_to_megabytes(os.path.getsize(save_path))
+
+    def save_image(image_quality=DEFAULT_QUALITY) -> tuple[str, int]:
+        if verbose: print('Attempting quality setting:', CURRENT_IMAGE_QUALITY)
+        image = Image.open(image_path)
+        save_path = os.path.join(
+            directory, ((f"COMPRESSED-QUAL={image_quality}_{filename}") + ".jpg")
+        )
+        jpg_image = image.convert("RGB")
+        jpg_image.save(save_path, quality=image_quality, optimize=True)
+        compressed_size = bytes_to_megabytes(os.path.getsize(save_path))
+        return save_path, compressed_size
+    
+    if CURRENT_IMAGE_QUALITY == DEFAULT_QUALITY:
+        save_path, compressed_size = save_image()
+    else:
+        save_path, compressed_size = save_image(image_quality=CURRENT_IMAGE_QUALITY)
+
+    SAVED_IMAGES.append(save_path)
+    if adaptive is not None:
+        if compressed_size > adaptive:
+            for CURRENT_IMAGE_QUALITY in (possible_quality_values := list(range(DEFAULT_QUALITY - 5, -1, -5))): #  already tried for first quality setting
+                save_path, compressed_size = save_image(image_quality = CURRENT_IMAGE_QUALITY)
+                SAVED_IMAGES.append(save_path)
+                if compressed_size < adaptive:
+                    break
+
     compression_size_drop = original_size - compressed_size
     compression_percent_decrease = (compression_size_drop / original_size) * 100
 
     if not suppress:
         if verbose:
-            print("Image compression level:", image_quality)
+            print("Image compression level:", CURRENT_IMAGE_QUALITY)
             print("Original image filetype:", extension)
             print("Original filesize:", str(round(original_size, 2)), "MB")
         print(
